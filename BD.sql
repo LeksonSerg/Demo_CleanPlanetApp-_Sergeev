197f17bfdf9b5754c19a5dcfbf55b9d9fe014d5b
LeksonSerg <sergeevaleksandr2007@gmail.com>
2025-10-23 12:57:23 +0300

    gg

--- BD.sql
+++ BD.sql
@@ -1,115 +1,159 @@
-USE [master]
+CREATE DATABASE CleanPlanet;
 GO
 
-/****** Object:  Database [CleanPlanet]    Script Date: 23.10.2025 12:15:41 ******/
-CREATE DATABASE [CleanPlanet]
- CONTAINMENT = NONE
- ON  PRIMARY 
-( NAME = N'CleanPlanet', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\CleanPlanet.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
- LOG ON 
-( NAME = N'CleanPlanet_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\CleanPlanet_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
- WITH CATALOG_COLLATION = DATABASE_DEFAULT
+USE CleanPlanet;
 GO
 
-IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
-begin
-EXEC [CleanPlanet].[dbo].[sp_fulltext_database] @action = 'enable'
-end
-GO
+CREATE TABLE Partners (
+    PartnerId INT IDENTITY(1,1) PRIMARY KEY,
+    PartnerType NVARCHAR(50) NOT NULL,
+    Name NVARCHAR(100) NOT NULL,
+    Director NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100) NOT NULL,
+    Phone NVARCHAR(20) NOT NULL,
+    LegalAddress NVARCHAR(200) NOT NULL,
+    INN NVARCHAR(20) NOT NULL,
+    Rating INT NOT NULL CHECK (Rating >= 0)
+);
 
-ALTER DATABASE [CleanPlanet] SET ANSI_NULL_DEFAULT OFF 
-GO
+CREATE TABLE ServiceTypes (
+    ServiceTypeId INT IDENTITY(1,1) PRIMARY KEY,
+    ServiceTypeName NVARCHAR(50) NOT NULL,
+    ComplexityCoefficient FLOAT NOT NULL
+);
 
-ALTER DATABASE [CleanPlanet] SET ANSI_NULLS OFF 
-GO
+CREATE TABLE Services (
+    ServiceId INT IDENTITY(1,1) PRIMARY KEY,
+    ServiceTypeId INT NOT NULL,
+    ServiceName NVARCHAR(100) NOT NULL,
+    ServiceCode NVARCHAR(20) NOT NULL,
+    MinimalCost DECIMAL(18,2) NOT NULL,
+    FOREIGN KEY (ServiceTypeId) REFERENCES ServiceTypes(ServiceTypeId)
+);
 
-ALTER DATABASE [CleanPlanet] SET ANSI_PADDING OFF 
-GO
+CREATE TABLE Materials (
+    MaterialId INT IDENTITY(1,1) PRIMARY KEY,
+    MaterialType NVARCHAR(50) NOT NULL,
+    OverheadPercent FLOAT NOT NULL
+);
 
-ALTER DATABASE [CleanPlanet] SET ANSI_WARNINGS OFF 
-GO
+CREATE TABLE PartnerServices (
+    PartnerServiceId INT IDENTITY(1,1) PRIMARY KEY,
+    PartnerId INT NOT NULL,
+    ServiceId INT NOT NULL,
+    Quantity INT NOT NULL,
+    ExecutionDate DATETIME NOT NULL,
+    FOREIGN KEY (PartnerId) REFERENCES Partners(PartnerId),
+    FOREIGN KEY (ServiceId) REFERENCES Services(ServiceId)
+);
 
-ALTER DATABASE [CleanPlanet] SET ARITHABORT OFF 
-GO
+INSERT INTO Partners (PartnerType, Name, Director, Email, Phone, LegalAddress, INN, Rating)
+VALUES 
+('ИП', 'Чистый Мир', 'Смирнова Ольга Викторовна', 'olga.smirnova@cm.ru', '495 111 22 33', '123456, г. Москва, ул. Чистая, д. 10', '1234567890', 9),
+('ООО', 'Блеск и Глянец', 'Комаров Денис Игоревич', 'denis.komarov@bg.ru', '812 444 55 66', '190000, г. Санкт-Петербург, Невский пр-т, д. 50', '2345678901', 8),
+('ИП', 'Свежесть Плюс', 'Николаева Анна Сергеевна', 'anna.nikolaeva@sp.ru', '383 777 88 99', '630000, г. Новосибирск, ул. Лазурная, д. 25', '3456789012', 7),
+('ЗАО', 'Уютный Дом', 'Фролов Артем Васильевич', 'artem.frolov@ud.ru', '843 222 33 44', '420000, г. Казань, ул. Уютная, д. 15', '4567890123', 6),
+('ООО', 'ЭкоТекстиль', 'Васнецова Ирина Петровна', 'irina.vasnecova@et.ru', '351 555 66 77', '454000, г. Челябинск, пр-т Победы, д. 100', '5678901234', 10);
 
-ALTER DATABASE [CleanPlanet] SET AUTO_CLOSE ON 
-GO
+INSERT INTO ServiceTypes (ServiceTypeName, ComplexityCoefficient)
+VALUES 
+('Стирка', 1),
+('Химчистка', 2.5),
+('Ремонт', 3),
+('Аквачистка', 1.8),
+('Сушка', 0.5);
 
-ALTER DATABASE [CleanPlanet] SET AUTO_SHRINK OFF 
-GO
+INSERT INTO Services (ServiceTypeId, ServiceName, ServiceCode, MinimalCost)
+VALUES 
+(1, 'Стирка постельного белья (до 10 кг)', 'SRV-001', 500),
+(1, 'Стирка верхней одежды (куртки, пальто)', 'SRV-002', 800),
+(2, 'Химчистка костюма', 'SRV-003', 1200),
+(2, 'Химчистка ковра (за кв.м)', 'SRV-004', 300),
+(3, 'Ремонт молнии на брюках', 'SRV-005', 250),
+(4, 'Аквачистка пуховика', 'SRV-006', 1500),
+(5, 'Сушка в барабанной сушилке (цикл)', 'SRV-007', 200);
 
-ALTER DATABASE [CleanPlanet] SET AUTO_UPDATE_STATISTICS ON 
-GO
+INSERT INTO Materials (MaterialType, OverheadPercent)
+VALUES 
+('Порошок', 0.05),
+('Растворитель', 0.08),
+('Отбеливатель', 0.1),
+('Кондиционер', 0.03),
+('Пятновыводитель', 0.07),
+('Упаковка', 0.02);
 
-ALTER DATABASE [CleanPlanet] SET CURSOR_CLOSE_ON_COMMIT OFF 
-GO
+INSERT INTO PartnerServices (PartnerId, ServiceId, Quantity, ExecutionDate)
+SELECT 
+    p.PartnerId,
+    s.ServiceId,
+    ps.Quantity,
+    ps.ExecutionDate
+FROM (
+    VALUES 
+    (1, 1, 150, '2023-03-23T00:00:00'),
+    (1, 3, 85, '2023-12-18T00:00:00'),
+    (1, 2, 120, '2024-06-07T00:00:00'),
+    (2, 4, 350, '2022-12-02T00:00:00'),
+    (2, 5, 45, '2023-05-17T00:00:00'),
+    (2, 1, 200, '2024-06-07T00:00:00'),
+    (2, 6, 30, '2024-07-01T00:00:00'),
+    (3, 1, 300, '2023-01-22T00:00:00'),
+    (3, 3, 60, '2024-07-05T00:00:00'),
+    (4, 2, 180, '2023-03-20T00:00:00'),
+    (4, 1, 250, '2024-03-12T00:00:00'),
+    (4, 6, 25, '2024-05-14T00:00:00'),
+    (5, 1, 500, '2023-09-19T00:00:00'),
+    (5, 2, 320, '2023-11-10T00:00:00'),
+    (5, 3, 150, '2024-04-15T00:00:00'),
+    (5, 4, 420, '2024-06-12T00:00:00')
+) AS ps(PartnerIndex, ServiceIndex, Quantity, ExecutionDate)
+INNER JOIN Partners p ON p.PartnerId = ps.PartnerIndex
+INNER JOIN Services s ON s.ServiceId = ps.ServiceIndex;
 
-ALTER DATABASE [CleanPlanet] SET CURSOR_DEFAULT  GLOBAL 
-GO
+CREATE TABLE ServiceMaterials (
+    ServiceMaterialId INT IDENTITY(1,1) PRIMARY KEY,
+    ServiceId INT NOT NULL,
+    MaterialId INT NOT NULL,
+    ConsumptionRate FLOAT NOT NULL, -- норма расхода на одну услугу
+    FOREIGN KEY (ServiceId) REFERENCES Services(ServiceId),
+    FOREIGN KEY (MaterialId) REFERENCES Materials(MaterialId)
+);
 
-ALTER DATABASE [CleanPlanet] SET CONCAT_NULL_YIELDS_NULL OFF 
-GO
+CREATE TABLE MaterialPrices (
+    MaterialPriceId INT IDENTITY(1,1) PRIMARY KEY,
+    MaterialId INT NOT NULL,
+    Price DECIMAL(18,2) NOT NULL, -- текущая цена
+    EffectiveDate DATETIME NOT NULL DEFAULT GETDATE(),
+    FOREIGN KEY (MaterialId) REFERENCES Materials(MaterialId)
+);
 
-ALTER DATABASE [CleanPlanet] SET NUMERIC_ROUNDABORT OFF 
-GO
+INSERT INTO MaterialPrices (MaterialId, Price) VALUES
+(1, 150.00), -- Порошок
+(2, 300.00), -- Растворитель
+(3, 200.00), -- Отбеливатель
+(4, 180.00), -- Кондиционер
+(5, 250.00), -- Пятновыводитель
+(6, 50.00);  -- Упаковка
 
-ALTER DATABASE [CleanPlanet] SET QUOTED_IDENTIFIER OFF 
-GO
+INSERT INTO ServiceMaterials (ServiceId, MaterialId, ConsumptionRate) VALUES
+-- Стирка постельного белья
+(1, 1, 0.1), -- Порошок
+(1, 4, 0.05), -- Кондиционер
+(1, 6, 1),   -- Упаковка
 
-ALTER DATABASE [CleanPlanet] SET RECURSIVE_TRIGGERS OFF 
-GO
+(2, 1, 0.15), -- Порошок
+(2, 4, 0.08), -- Кондиционер
+(2, 6, 1),    -- Упаковка
 
-ALTER DATABASE [CleanPlanet] SET  ENABLE_BROKER 
-GO
+(3, 2, 0.2),  -- Растворитель
+(3, 5, 0.05), -- Пятновыводитель
+(3, 6, 1),    -- Упаковка
 
-ALTER DATABASE [CleanPlanet] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
-GO
+(4, 2, 0.1),  -- Растворитель
+(4, 5, 0.02), -- Пятновыводитель
 
-ALTER DATABASE [CleanPlanet] SET DATE_CORRELATION_OPTIMIZATION OFF 
-GO
+(5, 6, 1),    -- Упаковка
 
-ALTER DATABASE [CleanPlanet] SET TRUSTWORTHY OFF 
-GO
-
-ALTER DATABASE [CleanPlanet] SET ALLOW_SNAPSHOT_ISOLATION OFF 
-GO
-
-ALTER DATABASE [CleanPlanet] SET PARAMETERIZATION SIMPLE 
-GO
-
-ALTER DATABASE [CleanPlanet] SET READ_COMMITTED_SNAPSHOT OFF 
-GO
-
-ALTER DATABASE [CleanPlanet] SET HONOR_BROKER_PRIORITY OFF 
-GO
-
-ALTER DATABASE [CleanPlanet] SET RECOVERY SIMPLE 
-GO
-
-ALTER DATABASE [CleanPlanet] SET  MULTI_USER 
-GO
-
-ALTER DATABASE [CleanPlanet] SET PAGE_VERIFY CHECKSUM  
-GO
-
-ALTER DATABASE [CleanPlanet] SET DB_CHAINING OFF 
-GO
-
-ALTER DATABASE [CleanPlanet] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
-GO
-
-ALTER DATABASE [CleanPlanet] SET TARGET_RECOVERY_TIME = 60 SECONDS 
-GO
-
-ALTER DATABASE [CleanPlanet] SET DELAYED_DURABILITY = DISABLED 
-GO
-
-ALTER DATABASE [CleanPlanet] SET ACCELERATED_DATABASE_RECOVERY = OFF  
-GO
-
-ALTER DATABASE [CleanPlanet] SET QUERY_STORE = OFF
-GO
-
-ALTER DATABASE [CleanPlanet] SET  READ_WRITE 
-GO
-
+(6, 1, 0.2),  -- Порошок
+(6, 3, 0.05), -- Отбеливатель
+(6, 6, 1);    -- Упаковка
